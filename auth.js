e932d3d08cf40cc64aa242eda0cc8fc713b7f1fc
anqixxxx <163583204+anqixxxx@users.noreply.github.com>
2024-06-27 14:24:54 +0800

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,15 +1,26 @@
-const signUpForm = document.querySelector('#signUpForm');
-signUpForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  // get user info
-  const email = signUpForm['signUpEmail'].value;
-  const password = signUpForm['signUpPassword'].value;
-  
-  // sign up the user
-  auth.createUserWithEmailAndPassword(email, password).then(cred => {
-    console.log(cred.user);
-    signUpForm.reset;
-    window.location.href = 'https://ppheartrate-5im9qopxv-anqis-projects-d8885076.vercel.app/';
-    }
-  )
-})
+// Import the necessary Firebase modules
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDf4yaXsG9ShvlAX4IVdGIPqPAypttqlPI",
+    authDomain: "pulsepals-fa684.firebaseapp.com",
+    projectId: "pulsepals-fa684",
+    storageBucket: "pulsepals-fa684.appspot.com",
+    messagingSenderId: "332323808828",
+    appId: "1:332323808828:web:926868c9d76ab22a8ac539",
+    measurementId: "G-ZLJM10C0BD"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+export { auth, db };
